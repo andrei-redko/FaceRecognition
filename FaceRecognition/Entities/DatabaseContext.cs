45a159a9aead183c2d0323f2113f8ab694552b4b
andrei-redko <andreiredko111@gmail.com>
2014-05-01 19:31:04 +0300

    rth

---
+++ FaceRecognition/Entities/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceRecognition.Entities
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Feature> Features { get; set; }
+    }
+}
